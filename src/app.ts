3e06831bb173ae19d1bfe701cf150fc5556c7966
MakoskiVictor <makoski.ed@gmail.com>
2024-04-06 19:28:31 -0300

    import routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import { router } from "./routes";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors({
   origin: '*'
 }));
+app.use(router)
 app.listen(PORT, ()=> console.log(
   `App listening on port ${PORT}`
 ))
